d110632c37a7523e845a4f706337b3eb5c2ed2e1
Bearcry55 <deepnarayanbanerjee@gmail.com>
2026-05-23 17:21:51 +0530

    name change to gosip

--- main.go
+++ main.go
@@ -1,72 +1,74 @@
 package main
+
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
-func main (){
-	// the program first greet the user the first interface 
-  fmt.Println("....welcome to ssshh the programe is started ... ")
-  fmt.Println("1. create  a CHAT ROOM  ")
-  fmt.Println("2. enter a room with CHAT ROOM ID   ")
-  fmt.Println("chose a option (1 or 2 ):  ")
-  //now need to variavble to store the option value 
-  reader := bufio.NewReader(os.Stdin) 
-  choice, err := reader.ReadString('\n')
-  if err!= nil {
-	fmt.Println("there some error in taking the input ")
-	return
-  }
-   choice = strings.TrimSpace(choice)
-  switch choice{
-  case "1":
-	fmt.Println("give me a username you like : ")
-	username,err:=reader.ReadString('\n')
-	if err!= nil {
-	fmt.Println("there some error in taking your username ")
-	return
-  }
-  username = strings.TrimSpace(username)
 
-  fmt.Println("give me a roomid you like : ")
-  roomid,err:=reader.ReadString('\n')
-	if err!= nil {
-	fmt.Println("there some error in taking your room id ")
-	return
-  }
-  roomid = strings.TrimSpace(roomid)
+func main() {
+	// the program first greet the user the first interface
+	fmt.Println("....welcome to Gosip the programe is started ... ")
+	fmt.Println("1. create  a CHAT ROOM  ")
+	fmt.Println("2. enter a room with CHAT ROOM ID   ")
+	fmt.Println("chose a option (1 or 2 ):  ")
+	//now need to variavble to store the option value
+	reader := bufio.NewReader(os.Stdin)
+	choice, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("there some error in taking the input ")
+		return
+	}
+	choice = strings.TrimSpace(choice)
+	switch choice {
+	case "1":
+		fmt.Println("give me a username you like : ")
+		username, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("there some error in taking your username ")
+			return
+		}
+		username = strings.TrimSpace(username)
 
-	 fmt.Println("creating a chatroom now with room id ... ")
-	 creatingchatroom(username ,roomid )
+		fmt.Println("give me a roomid you like : ")
+		roomid, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("there some error in taking your room id ")
+			return
+		}
+		roomid = strings.TrimSpace(roomid)
 
-  case "2":
-	fmt.Println("give me a username you like : ")
-	username,err:=reader.ReadString('\n')
-	if err!= nil {
-	fmt.Println("there some error in taking your username ")
-	return
-  }
-  username = strings.TrimSpace(username)
-  fmt.Println("give me a roomid you like to enter  : ")
-  roomid,err:=reader.ReadString('\n')
-	if err!= nil {
-	fmt.Println("there some error in taking your room id ")
-	return
-  }
-  roomid = strings.TrimSpace(roomid)
-  fmt.Println("enter room password: ")
-password, err := reader.ReadString('\n')
-if err != nil {
-    fmt.Println("error reading password")
-    return
-}
-password = strings.TrimSpace(password) 
-	fmt.Println("entering in the chatroom with room id... ")
-    fmt.Println("hi",username,"\n welcome to ssshh ")
-    joinchatroom(roomid ,username,password)
-  default: 
-  fmt.Println("\n  invalid option :( you choose ",choice ," please choose correct option . ")
-  }
+		fmt.Println("creating a chatroom now with room id ... ")
+		creatingchatroom(username, roomid)
+
+	case "2":
+		fmt.Println("give me a username you like : ")
+		username, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("there some error in taking your username ")
+			return
+		}
+		username = strings.TrimSpace(username)
+		fmt.Println("give me a roomid you like to enter  : ")
+		roomid, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("there some error in taking your room id ")
+			return
+		}
+		roomid = strings.TrimSpace(roomid)
+		fmt.Println("enter room password: ")
+		password, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("error reading password")
+			return
+		}
+		password = strings.TrimSpace(password)
+		fmt.Println("entering in the chatroom with room id... ")
+		fmt.Println("hi", username, "\n welcome to Gosip ")
+		joinchatroom(roomid, username, password)
+	default:
+		fmt.Println("\n  invalid option :( you choose ", choice, " please choose correct option . ")
+	}
 
 }
