21b5af58fee5625b0ccd3f33d0a3509fd02d735c
Deeop narayan Banerjee <deepnarayanbanerjee@gmail.com>
2026-06-09 19:39:11 +0530

    update the release script

--- release.sh
+++ release.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-
-# Usage: ./release.sh 1.1
-
+# Usage: ./release.sh 2.1
 VERSION=$1
-
 if [ -z "$VERSION" ]; then
-    echo "Usage: ./release.sh <version>"
-    echo "Example: ./release.sh 1.1"
-    exit 1
+echo "Usage: ./release.sh <version>"
+echo "Example: ./release.sh 2.1"
+exit 1
 fi
-
 echo "Building binaries for v$VERSION..."
 GOOS=linux GOARCH=amd64 go build -o gosip-linux
 GOOS=darwin GOARCH=amd64 go build -o gosip-mac
 GOOS=windows GOARCH=amd64 go build -o gosip.exe
 echo "Binaries built!"
-
 echo "Pushing to GitHub..."
 git add .
 git commit -m "release v$VERSION"
@@ -23,16 +18,14 @@ git tag v$VERSION
 git push origin main
 git push origin v$VERSION
 echo "Pushed to GitHub!"
-
 echo "Updating AUR..."
-cd ~/gosip
+cd ~/aur-gosip
 sed -i "s/pkgver=.*/pkgver=$VERSION/" PKGBUILD
 makepkg --printsrcinfo > .SRCINFO
 git add PKGBUILD .SRCINFO
 git commit -m "update to v$VERSION"
 git push
 echo "AUR updated!"
-
 echo ""
 echo "v$VERSION released!"
 echo "Now go to GitHub and upload the binaries to the release:"
